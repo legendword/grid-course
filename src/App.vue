65f7ca8e77aa82d277c0608cfd34a5dffa736b73
Legendword <zwh@legendword.com>
2022-04-29 13:30:02 -0700

    removed HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -28,14 +28,8 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
     name: 'App',
-
-    components: {
-        HelloWorld,
-    },
 
     data: () => ({
         //
--- src/course-lib/ubc-postprocessing.py
+++ src/course-lib/ubc-postprocessing.py
@@ -4,49 +4,20 @@ res = None
 with open('ubc-2021W.json', 'r') as f:
     res = json.loads(f.read())
 
-'''
+# nres = []
+lst = []
 for i in range(len(res)):
-    course_id = res[i]['subject'] + ' ' + res[i]['course']
-    res[i]['id'] = course_id
-    sections = []
-    section_ids = {}
+    section_types = []
     for j in res[i]['sections']:
-        section_id = course_id + ' ' + j['section']
-        j['id'] = section_id
-        if section_id in section_ids:
-            continue
-        section_ids[section_id] = True
-        sections.append(j)
-    res[i]['sections'] = sections
-'''
+        if j['type'] not in section_types:
+            section_types.append(j['type'])
+    if 'Distance Education' in section_types:
+        lst.append((res[i]['id'], section_types))
+    # if 'Lecture' in section_types and not section_types[0] == 'Lecture':
+    #     lst.append((res[i]['id'], section_types))
+    # nres.append(res[i])
 
-nres = []
-for i in range(len(res)):
-    course_id = res[i]['id']
-    sections = []
-    for j in res[i]['sections']:
-        section_id = j['id']
+# with open('ubc-2021W.json', 'w') as f:
+#     f.write(json.dumps(nres))
 
-        # if (not j['days']) or (len(j['days']) == 1 and j['days'][0] == ''):
-        #     continue
-
-        # if (not j['start_time']) or j['start_time'] == '' or (not j['end_time']) or j['end_time'] == '':
-        #     continue
-
-        if j['type'] == 'Waiting List':
-            continue
-        
-        sections.append(j)
-
-    if len(sections) == 0:
-        print('removed', course_id)
-        continue
-    elif not len(res[i]['sections']) == len(sections):
-        print(course_id, len(res[i]['sections']), '->', len(sections))
-    res[i]['sections'] = sections
-    nres.append(res[i])
-
-print(len(res), ' -> ', len(nres))
-
-with open('ubc-2021W.json', 'w') as f:
-    f.write(json.dumps(nres))
+print(lst)
